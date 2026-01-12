d3d025967a1041fef2e9ab0afe1f43c0cfd9c767
dobermai <>
2026-01-12 21:20:09 +0000

    Update sauna to 0.1.7

--- Formula/sauna.rb
+++ Formula/sauna.rb
@@ -1,17 +1,17 @@
 class Sauna < Formula
   desc "CLI tool for controlling Klafs saunas"
   homepage "https://github.com/dobermai/sauna"
-  version "0.1.6"
+  version "0.1.7"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_intel do
-      url "https://github.com/dobermai/sauna/releases/download/v0.1.6/sauna-macos-x86_64"
-      sha256 "6f978ceefb2e0dafbe28487db1363fa96e0a721405519e460435374807d59b48"
+      url "https://github.com/dobermai/sauna/releases/download/v0.1.7/sauna-macos-x86_64"
+      sha256 "7f3d92a9bc03442ae2d4efd6a3fcaec03bd980893ab38acf8fdf777ee42ed4e4"
     end
     on_arm do
-      url "https://github.com/dobermai/sauna/releases/download/v0.1.6/sauna-macos-aarch64"
-      sha256 "c07535a993d9b0ebd85caca71f7aea84d5a024e731e4663f5abe10108f55e8b8"
+      url "https://github.com/dobermai/sauna/releases/download/v0.1.7/sauna-macos-aarch64"
+      sha256 "1b37ef9f9661fc2b6731cb1bf49cf88f38595bb961f080926bfea6f52157adf3"
     end
   end
 
